2abd9da1b6251f4c54f613bab69bec416fa351a9
cheshire <garfield@snu.ac.kr>
2023-09-30 21:29:15 +0900

    findall제외하고 동작하게 수정

--- src/main/scala/example/UserApp.scala
+++ src/main/scala/example/UserApp.scala
@@ -9,28 +9,30 @@ private val userPath = Root / "api" / "v1" / "users"
 
 class UserApp(userRepo: UserRepo):
   val http: Http[Any, Nothing, Request, Response] =
-    Http.collectZIO[Request]:
-      case Method.GET -> userPath / id => userRepo.findById(id.toInt).map:
-        case Some(u) => Response.json(u.toJson)
-        case None => Response.status(Status.NotFound)
+    Http.collectZIO[Request] {
+      case Method.GET -> userPath / id =>
+        userRepo.findById(id.toInt).map {
+          case Some(u) => Response.json(u.toJson)
+          case None    => Response.status(Status.NotFound)
+        }
 
       case Method.GET -> userPath =>
-        (for
-         users <- userRepo.findAll
-         json = Util.jsonStringToList(users.map(_.toJson))
-        yield Response.json(json))
-
+        for users <- userRepo.findAll
+        yield Response.json(
+          Util.jsonToListString(users.map(_.toJson))
+        )
 
       case req @ Method.POST -> userPath =>
         for
           body <- req.body.asString.orDie
           user = User.fromJson(body)
-          _    <- userRepo.create(user)
+          _ <- userRepo.create(user)
         yield Response.json(user.toJson)
 
       case Method.DELETE -> userPath / id =>
         for _ <- userRepo.delete(id.toInt)
         yield Response.ok
+    }
 
 object UserApp:
   val layer: ZLayer[UserRepo, Nothing, UserApp] =
